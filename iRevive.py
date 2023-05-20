cdbfca93be6ab2bd0e809ab76fa211635a6430bc
Hacktivator <118433308+Hackt1vator@users.noreply.github.com>
2023-05-20 21:29:32 +0200

    Add files via upload

--- iRevive.py
+++ iRevive.py
@@ -33,15 +33,14 @@ frame = tk.Frame(root, width="500", height="250")
 frame.pack(fill=BOTH,expand=True)
 #tk.Entry(root).pack(fill='x')
 
-# uses current directory to load the image file for the icon
-root.iconphoto(False, tk.PhotoImage(file='irevive.png'))
+
 
 
 
 
 def show_popup():
     popup = tk.Toplevel()
-    popup.geometry("300x200")  # set the width and height of the window
+    popup.geometry("300x400")  # set the width and height of the window
     popup.title("palera1n")
 
     # center the window on the screen
@@ -61,7 +60,7 @@ def show_popup():
     
 def show_popup2():
     popup = tk.Toplevel()
-    popup.geometry("300x300")  # set the width and height of the window
+    popup.geometry("300x600")  # set the width and height of the window
     popup.title("bypass")
 
     # center the window on the screen
@@ -85,95 +84,30 @@ def show_popup2():
 
 def build_fakefs():
     messagebox.showinfo("", "Please put the device first in recovery mode and then into dfu mode after the device is in dfu click ok")
-    # Display a message box with a yes/no option
-    response = messagebox.askyesno("iphone?", "Do you have a A9 device?")
-
-    # Check the user's response and execute the appropriate command
-    if response == 1:  # Yes
-        # Run the command in a subprocess
-        process = subprocess.Popen(["./palera1n-macos-universal", "-cf"], preexec_fn=os.setsid)
-
-        # Wait for 8 seconds or until the process completes
-        for i in range(20):
-            if process.poll() is not None:
-                break
-            time.sleep(1)
-
-        # If the process is still running, terminate it
-        if process.poll() is None:
-            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-
-
-        os.system("./palera1n-macos-universal -cf")
-    if response == 0:  # No
-        os.system("./palera1n-macos-universal -cf")
+ 
+    os.system("sudo ./palera1n-linux-x86_64 -cf")
     messagebox.showinfo("", "After the device boots you can boot the fake fs")
     
 def boot_fakefs():
     messagebox.showinfo("", "Please put the device first in recovery mode and then into dfu mode after the device is in dfu click ok")
-    # Display a message box with a yes/no option
-    response = messagebox.askyesno("iphone?", "Do you have a A9 device?")
-
-    # Check the user's response and execute the appropriate command
-    if response == 1:  # Yes
-        # Run the command in a subprocess
-        process = subprocess.Popen(["./palera1n-macos-universal", "-f"], preexec_fn=os.setsid)
-
-        # Wait for 8 seconds or until the process completes
-        for i in range(20):
-            if process.poll() is not None:
-                break
-            time.sleep(1)
-
-        # If the process is still running, terminate it
-        if process.poll() is None:
-            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-
-
-        os.system("./palera1n-macos-universal -f")
-    if response == 0:  # No
-        os.system("./palera1n-macos-universal -f")
+ 
+    os.system("sudo ./palera1n-linux-x86_64 -f")
     messagebox.showinfo("", "Now start the bypass")
-
-
-
-
 
 def startcheckra1n():
 
 
-
-    os.system("open ./checkra1n.app")
+    messagebox.showinfo("", "Please put the device first in recovery mode and then into dfu mode after the device is in dfu click ok")
+    os.system("./checkra1n -c -V -E")
 
 
 
 
     
 
-def enterRecMode():
-
-    os.system("./device/ideviceinfo > ./device/lastdevice.txt")
-    time.sleep(2)
-
-    f = open("./device/lastdevice.txt", "r")
-    fileData = f.read()
-    f.close()
-    # Find the UDID
-    start = 'UniqueDeviceID: '
-    end = 'UseRaptorCerts:'
-    s = str(fileData)
-
-    foundData = s[s.find(start) + len(start):s.rfind(end)]
-    UDID = str(foundData)
-    LAST_CONNECTED_UDID = str(UDID)
-    print("Kicking device into recovery mode...")
-    os.system(f"./device/ideviceenterrecovery {LAST_CONNECTED_UDID}")
-
 def exitRecMode():
     print("Kicking device out recovery mode...")
     os.system("./device/irecovery -n")
-
-
 
 def quitProgram():
     print("Exiting...")
@@ -249,7 +183,7 @@ frame.pack()
 
 #BIG title on program
 mainText = Label(root, text="iRevive",font=('Helveticabold', 24))
-mainText.place(x=310, y=170)
+mainText.place(x=310, y=400)
 
 img2 = Image.open("iRevive.jpg")
 NewIMGSize2 = img2.resize((1000,1000), Image.ANTIALIAS)
@@ -260,12 +194,12 @@ label.place(x=0, y=0)
 #label
 my_label2 = Label(frame,
                  text = "passcode Bypass")
-my_label2.place(x=300, y=230)
+my_label2.place(x=300, y=500)
 
 #label
 my_label3 = Label(frame,
                  text = "ver 1.1")
-my_label3.place(x=10, y=420)
+my_label3.place(x=10, y=700)
 
 
 
@@ -273,36 +207,31 @@ cButton1 = tk.Button(frame,
                    text="bypass ios 12-16.5",
                    command=show_popup2,
                    state="normal")
-cButton1.place(x=270, y=360)
-cButton5 = tk.Button(frame,
-                   text="enter recovery",
-                   command=enterRecMode,
-                   state="normal")
-cButton5.place(x=10, y=10)
+cButton1.place(x=270, y=600)
 cButton6 = tk.Button(frame,
                  text="exit recovery",
                  command=exitRecMode,
                  state="normal")
-cButton6.place(x=580, y=10)
+cButton6.place(x=780, y=10)
 cButton7 = tk.Button(frame,
                    text="start checkra1n",
                    command=startcheckra1n,
                    state="normal")
-cButton7.place(x=530, y=200)
+cButton7.place(x=700, y=400)
 cButton9 = tk.Button(frame,
                    text="start palera1n",
                    command=show_popup,
                    state="normal")
-cButton9.place(x=50, y=200)
+cButton9.place(x=50, y=400)
 
 
 cbeginExploit2 = tk.Button(frame,
                    text="Twitter!",
                    command=opentwitter,
                    state="normal")
-cbeginExploit2.place(x=580, y=410)
+cbeginExploit2.place(x=850, y=700)
 
-root.geometry("700x450")
+root.geometry("1000x800")
 
 root.resizable(False, False)
 
